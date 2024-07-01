6a76f62bb998b97fe00a3dc2eb08f5b595bd8aa2
Yoosup Shin <brian96shin@gmail.com>
2024-07-01 16:35:21 +0900

    1장 :: 중복 코드의 메소드 추출

--- src/main/java/com/yoosup/springstudy/domain/user/UserDao.java
+++ src/main/java/com/yoosup/springstudy/domain/user/UserDao.java
@@ -4,11 +4,8 @@ import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/springStudy", "root", "ss0920"
-        );
-
+        Connection c = getConnection();
+        
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values (?,?,?)");
         ps.setString(1, user.getId());
@@ -22,9 +19,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException{
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/springStudy", "root", "ss0920");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?");
@@ -44,6 +39,12 @@ public class UserDao {
         return user;
     }
 
+    private Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306", "root", "ss0920");
+        return c;
+    }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao dao = new UserDao();
