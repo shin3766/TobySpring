cf1824194f09dc46cbe23d0a48a0a363660330f2
Yoosup Shin <brian96shin@gmail.com>
2024-07-02 17:11:55 +0900

    1장 :: DB커넥션 만들기의 독립:: DaoFactory 스프링 빈 팩토리 적용

--- src/main/java/com/yoosup/springstudy/domain/user/DaoFactory.java
+++ src/main/java/com/yoosup/springstudy/domain/user/DaoFactory.java
@@ -1,9 +1,25 @@
 package com.yoosup.springstudy.domain.user;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
 public class DaoFactory {
+    @Bean // 오브젝트 생성을 담당하는 IoC용 메소드라는 표시
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-        return userDao;
+        return new UserDao(connectionMaker());
+    }
+
+//    public AccountDao accountDao() {
+//        return new AccountDao(connectionMaker());
+//    }
+//
+//    public MessageDao messageDao() {
+//        return new MessageDao(connectionMaker());
+//    }
+
+    @Bean
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
     }
 }
