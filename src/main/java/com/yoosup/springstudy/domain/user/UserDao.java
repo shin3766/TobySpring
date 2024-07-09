1abe7d8091e4b483d7ce90a1089425850f27b9fc
Yoosup Shin <brian96shin@gmail.com>
2024-07-09 13:11:38 +0900

    3장 :: 예외처리:: add() 익명 클래스로 리팩토링

--- src/main/java/com/yoosup/springstudy/domain/user/UserDao.java
+++ src/main/java/com/yoosup/springstudy/domain/user/UserDao.java
@@ -12,21 +12,17 @@ public class UserDao {
     }
 
     public void add(final User user) throws SQLException {
-        class AddStatement implements StatementStrategy {
+        StatementStrategy st = new StatementStrategy() {
             public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
                 PreparedStatement ps =
                         c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-
                 ps.setString(1, user.getId());
                 ps.setString(2, user.getName());
                 ps.setString(3, user.getPassword());
 
                 return ps;
             }
-        }
-        StatementStrategy st = new AddStatement();
-        jdbcContextWithStatementStrategy(st);
-
+        };
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
@@ -51,8 +47,14 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        StatementStrategy st = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(st);
+        jdbcContextWithStatementStrategy(
+                new StatementStrategy() {
+                    @Override
+                    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                        return c.prepareStatement("delete from users");
+                    }
+                }
+        );
     }
 
     public class DeleteAllStatement implements StatementStrategy {
