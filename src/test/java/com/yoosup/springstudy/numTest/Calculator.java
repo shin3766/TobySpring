e2afc2479602e2eda699485aa43e1fa7429a4d71
Yoosup Shin <brian96shin@gmail.com>
2024-07-11 12:27:37 +0900

    3장 :: 예외처리:: BufferedReaderCallback을 사용하는 템플릿 /콜백 패턴으로 리팩토링

--- src/test/java/com/yoosup/springstudy/numTest/Calculator.java
+++ src/test/java/com/yoosup/springstudy/numTest/Calculator.java
@@ -6,14 +6,11 @@ import java.io.IOException;
 import java.nio.Buffer;
 
 public class Calculator {
-    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
+    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException{
         BufferedReader br = null;
-
         try {
             br = new BufferedReader(new FileReader(filepath));
-
             int ret = callback.doSomethingWithReader(br);
-
             return ret;
         } catch (IOException e) {
             System.out.println(e.getMessage());
@@ -21,10 +18,24 @@ public class Calculator {
         } finally {
             if (br != null) {
                 try { br.close(); }
-                catch ( IOException e ) {
-                    System.out.println(e.getMessage());
-                }
+                catch (IOException e) { System.out.println(e.getMessage()); }
             }
         }
+    }
+
+    public Integer calcSum(String filepath) throws IOException {
+        BufferedReaderCallback sumCallback
+                = new BufferedReaderCallback() {
+            @Override
+            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                Integer sum = 0;
+                String line = null;
+                while((line = br.readLine()) != null) {
+                    sum += Integer.valueOf(line);
+                }
+                return sum;
+            }
+        };
+        return fileReadTemplate(filepath, sumCallback);
     }
 }
